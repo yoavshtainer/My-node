f17f909cce7b1fa5db436444c0a76950c60a06a7
yoavshtainer <yoavshtainer@gmail.com>
2017-02-06 10:52:07 +0200

    Add files via upload

--- MyServer.js
+++ MyServer.js
@@ -1,38 +1,31 @@
 var fs = require("fs");
 var express = require("express");
+var bodyParser = require("body-parser");
+
 
 var config = JSON.parse(fs.readFileSync("config.json"));
 var host = config.host;
 var port = config.port;
 
+var names = [];
 var app = express();
+
+app.use(bodyParser.urlencoded());
+
 console.log("start");
 app.get("/", function(request, response){
 	var content = fs.readFileSync("index.html");
 	
-	// getTweets(function(tweets){
-	// 	var ul = '';
-	// 	tweets.forEach(function(tweet) {
-	// 		ul += "<li><strong>" + tweet.user.screen_name + ": </strong>" + tweet.text + "</li>";
-	// 	});
-	// 	content = content.toString("utf8").replace("{{INITIAL_TWEETS}}", ul);
 		response.setHeader("Content-Type", "text/html");
 		response.send(content);
-	// });
+
 });
 
-app.get("/1", function(request, response){
-	var content = fs.readFileSync("index1.html");
-	console.log("change");
-	// getTweets(function(tweets){
-	// 	var ul = '';
-	// 	tweets.forEach(function(tweet) {
-	// 		ul += "<li><strong>" + tweet.user.screen_name + ": </strong>" + tweet.text + "</li>";
-	// 	});
-	// 	content = content.toString("utf8").replace("{{INITIAL_TWEETS}}", ul);
-		response.setHeader("Content-Type", "text/html");
-		response.send(content);
-	// });
+app.post("/", function(request, response){
+  	console.log("POST " + request.body.txtName);
+	names.push( " " + request.body.txtName);
+  response.send('api/actionName ' +  names + ".");
+
 });
 
 app.listen(port, host);
